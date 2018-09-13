56e3766182e16fd4d3adba338083fba610336949
Nao YONASHIRO <owan.orisano@gmail.com>
2018-09-13 23:15:52 +0900

    feat: initial commit

---
+++ command.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
+
+type Command interface {
+	FlagSet() *flag.FlagSet
+	Run(args []string) error
+}
+
+var ErrInvalidArguments = errors.New("invalid arguments")
+
+func SubCommand(args []string, commands []Command) error {
+	if len(args) == 0 {
+		return fmt.Errorf("required subcommand")
+	}
+	subCommand := args[0]
+
+	for _, command := range commands {
+		flagSet := command.FlagSet()
+		if flagSet.Name() != subCommand {
+			continue
+		}
+		if err := flagSet.Parse(args[1:]); err != nil {
+			return err
+		}
+		err := command.Run(flagSet.Args())
+		if err == ErrInvalidArguments && flagSet.Usage != nil {
+			flagSet.Usage()
+		}
+		return err
+	}
+	return fmt.Errorf("subcommand not found: %q", subCommand)
+}
